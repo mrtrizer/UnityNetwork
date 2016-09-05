17929e69cb0b376d16fe9574bc76a367b15cd5d6
trizer <mrtrizer@gmail.com>
2016-09-06 01:09:53 +0300

    Initial commit.

---
+++ Assets/Apple.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Apple : NetworkBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (isServer)
+			RpcRespawn ();
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isServer)
+		{
+			Camera cam = Camera.main;
+			transform.position = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
+		}
+	}
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+	[SerializeField]
+	private float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<SpriteRenderer>().color = Color.red;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+
+		Camera.main.transform.position = new Vector3 (transform.position.x, transform.position.y, -10.0f);
+
+		if (Input.GetMouseButton (0)) {
+			var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			var dir = new Vector3 (mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+			GetComponent<Rigidbody2D> ().velocity = dir.normalized * speed;
+		} else {
+			GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		}
+	}
+
+}
